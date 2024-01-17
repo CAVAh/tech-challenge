6c92138cf68bd184cdc2aa526a7a9923dd166524
Matheus <30qr4xnl@duck.com>
2024-01-17 13:04:08 -0300

    crud-product: criando entidades de dominio para produto e categoria de produto

---
+++ src/core/domain/entities/product.go
@@ -0,0 +1,21 @@
+package entities
+
+type Product struct {
+	ID          uint    `json:"id"`
+	Name        string  `json:"name"`
+	Price       float64 `json:"price"`
+	Description string  `json:"description"`
+	CategoryID  uint    `json:"categoryId"`
+	CreatedAt   string  `json:"createdAt"`
+}
+
+func (p Product) IsExistingProduct() bool {
+	return p.ID > 0
+}
+
+func (p *Product) PatchFields(name string, price float64, description string, categoryId uint) {
+	p.Name = name
+	p.Price = price
+	p.Description = description
+	p.CategoryID = categoryId
+}
---
+++ src/core/domain/entities/product_category.go
@@ -0,0 +1,6 @@
+package entities
+
+type ProductCategory struct {
+	ID          uint   `json:"id"`
+	Description string `json:"description"`
+}
