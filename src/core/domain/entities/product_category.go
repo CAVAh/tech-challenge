a1588ed7fa3d1ca7d733fcd365533693ce78feca
Matheus <30qr4xnl@duck.com>
2024-01-18 20:17:36 -0300

    crud-product: criando validação entidade product_category.go para id existente

--- src/core/domain/entities/product_category.go
+++ src/core/domain/entities/product_category.go
@@ -4,3 +4,7 @@ type ProductCategory struct {
 	ID          uint   `json:"id"`
 	Description string `json:"description"`
 }
+
+func (pc ProductCategory) IsExistingProductCategory() bool {
+	return pc.ID > 0
+}
