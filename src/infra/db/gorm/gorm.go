faa1e1446aa317ba54588a11d7f8529431facb98
Lucas Cava <lucas.cava@hotmail.com>
2024-03-07 20:49:01 -0300

    refactor: remove comentário para ambiente local

--- src/infra/db/gorm/gorm.go
+++ src/infra/db/gorm/gorm.go
@@ -25,10 +25,6 @@ func ConnectDB() {
 		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
 	DB, err = gorm.Open(postgres.Open(conectionString))
 
-	// Comentado para utilizar localmente
-	//conectionString := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable TimeZone=America/Fortaleza"
-	//DB, err = gorm.Open(postgres.Open(conectionString))
-
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
